54aaffe47696ff895a9916c140dd8e3661a217fa
Justin O'Neill <Justin@Oneezy.localdomain>
2017-08-15 08:57:56 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+/*
+    App
+---------------------------------------------------------------------------------- */
+
+$(document).ready(function() {
+
+
+
+    $( ".tabs" ).tabs({
+      active: 0
+    });
+
+    $( ".collapsible" ).accordion();
+
+    setTimeout(function(){
+      $(".splash").fadeOut();
+    }, 2000);
+
+
+    // Cache 'spots' for DOM
+    var $spots = $('#Spots');
+
+    $.ajax({
+        type: 'GET',
+        url: '/js/map.json',
+        success: function(spots) {
+            $.each(spots, function(i, spot) {
+                $spots.append('<li>name: test</li>');
+            });
+        }
+    });
+
+
+
+});
